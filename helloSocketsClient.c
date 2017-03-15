51c23c6e1cce2b400c4276666e5c45c766980461
Bonnie <kwongb@eos-class.engr.oregonstate.edu>
2017-03-14 21:54:43 -0700

    added file size char check to client

--- helloSocketsClient.c
+++ helloSocketsClient.c
@@ -3,6 +3,7 @@
 #include <netdb.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv){
 
@@ -11,8 +12,65 @@ int main(int argc, char **argv){
  char recvline[1000];
  struct sockaddr_in servaddr;
 
- // get portNumber from argv
- int portNumber = atoi(argv[1]);
+ // get portNumber from argv at index 3
+ int portNumber = atoi(argv[3]);
+
+
+ // open and check plaintext file and key File
+ // Do character validaton
+ FILE *plainText = fopen(argv[1], "r");
+ if(plainText == NULL){
+  printf("no such file \n");
+ }
+
+
+ // check that the key file is valid
+ FILE *keyFile = fopen(argv[2], "r");
+ if(keyFile == NULL){
+  printf("no such key file\n");
+ }
+
+ // compare char count for each file
+ int countPlainText = 0;
+ int countKey = 0;  // initialize to 0
+
+ /*  character or EOF flag */
+ int ch;
+ 
+ while(1){
+  ch = fgetc(plainText);
+  /* If EOF is detected, break*/
+  if(ch == EOF){
+   break;
+  }
+  // otherwise add to count
+  ++countPlainText;
+ }
+
+ printf("the plain text count is %d\n", countPlainText);
+ 
+ /* Count the number of chars in the key file*/
+ int keyCounter; 
+ while(1){
+  keyCounter = fgetc(keyFile);
+  /* If EOF is detected, break */
+  if(keyCounter == EOF){
+   break;
+  }
+  // otherwise add to the count
+  ++countKey;
+ }
+ printf("The key char count is %d\n", countKey);
+ 
+ /* If the char count of key is less than
+ * the char count of plainText, show error to user
+ * and exit with an error and set exit value to 1  */
+ if(countKey < countPlainText){
+  fprintf(stderr, "Error: key '%s' is too short\n", argv[2]);
+  exit(1);
+ }
+
+
 
  sockfd=socket(AF_INET, SOCK_STREAM, 0);
  bzero(&servaddr, sizeof servaddr);
