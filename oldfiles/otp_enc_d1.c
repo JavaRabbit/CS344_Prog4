0bd7135516300fa1cc8265557c7fa374d8515b38
Bonnie <kwongb@eos-class.engr.oregonstate.edu>
2017-03-16 01:22:24 -0700

    working, updated server.c for spaces.

--- oldfiles/otp_enc_d1.c
+++ oldfiles/otp_enc_d1.c
@@ -7,7 +7,7 @@
 #include <sys/types.h>
 #include <netdb.h>
 #include <string.h>
-
+/*  SERVER  for ENCODING   */
 // prototypes
 void doprocessing(int sock);
 
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]){
 
  // file descriptors to be used
  int sock_fd, newsockfd, connection_fd, client;
-
+ 
  // struct to hold IP address and port numbers
  struct sockaddr_in server, client_addr;
  int len;
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]){
  // assign portNumber to argv[1]
  portNumber = atoi(argv[1]);
 
- //  Call to socket function
+ //  Call to socket function 
  if((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
   perror("socket: ");
  }
@@ -40,26 +40,26 @@ int main(int argc, char *argv[]){
 
  // set addressing scheme to AF_INET  (IP)
  server.sin_family = AF_INET;
-
+ 
  //  Listen on port specified port
  server.sin_port = htons(portNumber);  // port number
-
+ 
  // Allow any IP to connect
  server.sin_addr.s_addr = htons(INADDR_ANY);
 
  // Bind the host address
- bind(sock_fd, (struct sockaddr *) &server, sizeof(server));
-
+  bind(sock_fd, (struct sockaddr *) &server, sizeof(server));
+ 
  // start to listen. Allow at most 5 connections
- listen(sock_fd, 5);
-
+ listen(sock_fd, 5);  
+ 
  client = sizeof(client_addr);
 
  while(1){
   connection_fd = accept(sock_fd, (struct sockaddr*) &client_addr, &client);
 
   //  for each, for off a new process
-  pid = fork();
+  pid = fork(); 
 
   /* This is the child process*/
   if(pid ==0){
@@ -76,54 +76,17 @@ int main(int argc, char *argv[]){
 } // end of main
 
 void doprocessing(int sock){
-  int n,p;
+  int n,p; 
   char str[1000];
-  char keyStr[1000];
+  char keyStr[1000];   
   bzero(str,1000);
   bzero(keyStr,1000);
-
+   
   n = read(sock, str, 1000);
   p = read(sock, keyStr,1000);
 
-  //bzero(str,1000);
-  //bzero(keyStr, 1000);
-  //read(connection_fd, str, 1000);
-  //read(connection_fd, keyStr, 1000);
-  //printf("Echo,", str);
-
   //  send back to the client
   // write(connection_fd, keyStr, strlen(str)+1);
-  
-  // string to hold the cipher. length should be str length
-  char cipherStr[strlen(str)+1];
-  int i;
-  for(i = 0; i < strlen(str); i++){
-    int valStr;
-    if(str[i] == 32){  // its a space
-      valStr = 27;
-    } else{  /* not a space so just -65 */
-       valStr  = str[i] - 65;  // don't forget if space
-    }
-    int valKey;
-    if(keyStr[i] == 32){
-      valKey = 27;
-    } else {
-      valKey = valKey = 65;
-   }
-    
-    // sum up the message and the key
-    int total = valStr + valKey;
-
-    // check if the total is 27. If it is, reassign to 32
-    if(total == 27){
-      cipherStr[i] = 32;  // meaning that it will code to a space
-    } else {
-      cipherStr[i] = (total %27) + 65;
-    }
-  }
-
-
-
-  write(sock, cipherStr, strlen(keyStr)+1);
+  write(sock, str, strlen(keyStr)+1);
 
 }
